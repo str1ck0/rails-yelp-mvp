b49714cab8e584a8f731cb0d1bb1c1914e37e68b
Liam Strickland <liam.strickland96@gmail.com>
2022-10-26 13:20:42 +0200

    ARCH/ made restaurants controller, view, routes

--- Gemfile
+++ Gemfile
@@ -3,6 +3,10 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.1.2"
 
+gem "faker"
+
+gem "simple_form", github: "heartcombo/simple_form"
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+10.times do
+  Restaurant.create(name: Faker::Restaurant.name,
+                    address: Faker::Address.street_address,
+                    category: %w[chinese italian japanese french belgian].sample,
+                    phone_number: Faker::PhoneNumber.phone_number)
+end
+
+puts "Finished! #{Restaurant.count} restaurants created"
